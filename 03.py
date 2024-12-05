ccdd3fe8d6f6e6a7b03e22847e9f8cbe88fe0aef
Assoees <assoee@gmail.com>
2024-12-05 15:42:13 +0200

    Create 03.py

---
+++ 03.py
@@ -0,0 +1,14 @@
+# 03 Ülesanne
+# Asso Eesmäe 05.12.24
+
+
+#3.1
+nimi = "Asso" #sõne, string
+vanus = 33 #int, integer, täisarv
+keskmine_hinne = 5.5 #komaarv, float
+#plussiga saan stringid kokku
+print(nimi+", "+str(vanus)+" aastat vana ja keskmine hinne "+str(keskmine_hinne))
+#komaga saan mitut asja printida
+print(nimi,",",vanus, "aastat vana ja kesmine hinne", keskmine_hinne)
+#lause vormindamine
+print(f"{nimi}, on {vanus} aastat vana ja keskmine hinne on {keskmine_hinne}!")
