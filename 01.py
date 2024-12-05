0cd4c24cbcf1bf3bdc5ca9b078bfed007c6a3b73
Assoees <assoee@gmail.com>
2024-12-05 14:40:10 +0200

    01 ülesanne
    Pyton kolmnurk

---
+++ 01.py
@@ -0,0 +1,20 @@
+#01. Ülesnnae
+#Asso Eesmäe 05.12.24
+
+#Impordib kilpkonna mooduli
+import turtle
+
+#näita kilpkonna, avab graafilise akna
+turtle.showturtle()
+#muudab kuvatava kilpkonna visuaali(classic, turtle, arrow, circle)
+turtle.shape("turtle")
+#liigub otse edasi (ühikut), lühem käsk: turtle.fd
+turtle.forward(200)
+#liigutab kilpkonna vasakule(mitu kraadi), lühem: turtle.lt
+turtle.left(120)
+turtle.forward(200)
+turtle.left(120)
+turtle.forward(200)
+turtle.lt(120)
+turtle.done()
+
