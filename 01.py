2df37ef57bc9359f80a769cf8511253193045313
Assoees <assoee@gmail.com>
2024-12-05 14:53:14 +0200

    Update 01.py

--- 01.py
+++ 01.py
@@ -16,5 +16,25 @@ turtle.forward(200)
 turtle.left(120)
 turtle.forward(200)
 turtle.lt(120)
+
+#süda
+
+#tõstab kilpkonna üles, et ei joonistaks
+turtle.penup()
+#liigutab kilpkonna soovitud kohale
+turtle.goto(-300,0)
+#paneb kilkonna tagasi, et joonistaks
+turtle.pendown()
+#liigutab kilpkonna edasi
+turtle.fd(100)
+#joonistab poolringi
+turtle.circle(50,180)
+#keerab kilpkonna paremale
+turtle.rt(90)
+turtle.circle(50,180)
+turtle.fd(100)
+#keerabkilpkonna vasakule
+turtle.lt(90)
+#kilpkonna peatamine, et ei jokseks graafiline aken kokku
 turtle.done()
 
