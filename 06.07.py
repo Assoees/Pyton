50dfd732f68f125e7f78e6214c812f4660c33cd7
Assoees <assoee@gmail.com>
2024-12-19 17:34:16 +0200

    Create 06.07.py
    Metshein.com lehel pyton/turtle ülesanne

---
+++ 06.07.py
@@ -0,0 +1,12 @@
+#19.12.24
+#Eesmae
+#Ülesanne 07
+
+nimi = ["Jyri","Martin","Andres","Juuli","Maali"]
+
+for i in nimi:
+    
+    print(i)
+    
+for i in range(4):
+        print(f"{i+1}. {nimi[i]}")
