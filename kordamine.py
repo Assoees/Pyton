f6ac7622c69ba0f890e4d401f71a430b5637724a
Assoees <132949073+Assoees@users.noreply.github.com>
2025-03-13 20:43:16 +0200

    Update kordamine.py

--- kordamine.py
+++ kordamine.py
@@ -13,19 +13,55 @@
 
 
 
+# #	    Leia, kui palju on broneeringuid teenusele "Massaaž".
 import requests
 import json
-
+import datetime
+#Algselt on 0 massaaži
+massaaz = 0
 #Lehekülg kus fail asub
 url = 'https://metshein.com/kordamine/json/broneeringud.json'
-response = requests.get(url).json()
+response = requests.get(url)
+if response.status_code == 200:
+    data = response.json()
+    # for broneering in data["broneeringud"]:
+    #     if broneering["teenus"]=="Massaaž":
+    #         massaaz += 1
+    # print(massaaz)
+
+# #     Loetle unikaalsed teenused ja mitu korda neid broneeriti.
+    # teenused = {}
+    # for broneering in data["broneeringud"]:
+    #     teenus = broneering["teenus"]
+    #     if teenus in teenused:
+    #         teenused[teenus] += 1
+    #     else:
+    #         teenused[teenus] = 1
+    # print(teenused)
+
+    
+ # #	Leia kõik broneeringud, mis toimuvad pärast kella 12:00.       
+    
+
+# tänane kuupäev
+    täna = datetime.datetime.now()
+# praegune aeg
+    praegu = täna.time()
+# broneeringud pärast 12:00
+    for broneering in data["broneeringud"]:
+        aeg = datetime.datetime.strptime(broneering["aeg"], "%H:%M").time()
+        if aeg > datetime.time(12, 0):
+         print(broneering) 
 
 
-# if response.status_code == 200:
-    # data = response.json()
-massaaž = [b for b in response if b["teenus"] == "Massaaž"]
-    # for broneering in data:
-    #     if broneering ["teenus" == "Massaaž"]:
-print(f"Massaaži bron. arv: {len(massaaž)}")
-# else:
-#     print(f"Faili alla laadimine ebaõnnestus: {response.status_code}")
+
+
+
+
+
+
+
+
+
+else:
+    print("Päring ebaõnnestus, staatuskood:", response.status_code)
